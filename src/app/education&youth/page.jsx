1c16a167d748e7c68e8934f6c01f5a84dc85d09f
monu rajj <monu2feb2004@gmail.com>
2025-03-10 22:50:24 +0530

    Update page.jsx

--- src/app/education&youth/page.jsx
+++ src/app/education&youth/page.jsx
@@ -333,14 +333,14 @@ const EducationYouth = () => {
                       📝 Registration
                     </h5>
                     <p className="text-gray-700">{competition.registration}</p>
-                    <a
+{/*                     <a
                       href={competition.registrationLink}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="mt-4 inline-block px-6 py-2 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition-all"
                     >
                       Register Now 🚀
-                    </a>
+                    </a> */}
                   </div>
 
                   {/* Important Dates */}
